845363080125104f91bcd301e88474c4b47be993
ekgh2002 <ekgh2002@naver.com>
2022-09-22 15:21:16 +0900

    first commit

---
+++ tcpClient/build/CMakeFiles/tcpClient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tcpClient.dir/main.cpp.o"
+  "CMakeFiles/tcpClient.dir/main.cpp.o.d"
+  "tcpClient"
+  "tcpClient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tcpClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tcpClient/build/CMakeFiles/tcpServer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tcpServer.dir/main.cpp.o"
+  "CMakeFiles/tcpServer.dir/main.cpp.o.d"
+  "tcpServer"
+  "tcpServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tcpServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
